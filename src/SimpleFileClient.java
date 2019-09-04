ac7d2291f3404ac3f7559d0c76ff19a426cb9b79
nicolasmilholi <milholi51@gmail.com>
2019-09-04 15:59:34 -0300

    meudessssssssssssssss

--- src/SimpleFileClient.java
+++ src/SimpleFileClient.java
@@ -9,60 +9,113 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class SimpleFileClient {
 
-	public final static int SOCKET_PORT = 13267; // you may change this
-	public final static String SERVER = "localhost"; // localhost
-	public final static String FILE_TO_RECEIVED = "\\Users\\nicol\\Desktop\\ImagemSerializedeucetocarai2.ser"; // you
+	public static void main(String[] args) {
+		new Thread(t1).start();
+		new Thread(t2).start();
 
-	public final static int FILE_SIZE = 6022386; // file size temporary hard coded
-	// should bigger than the file to be downloaded
-
-	public static void main(String[] args) throws IOException {
-		
-		int bytesRead;
-		int current = 0;
-		FileOutputStream fos = null;
-		BufferedOutputStream bos = null;
-		Socket sock = null;
-		try {
-			sock = new Socket(SERVER, SOCKET_PORT);
-
-			System.out.println("Connecting...");
-
-			// receive file
-			byte[] mybytearray = new byte[FILE_SIZE];
-			InputStream is = sock.getInputStream();
-			fos = new FileOutputStream(FILE_TO_RECEIVED);
-			bos = new BufferedOutputStream(fos);
-			bytesRead = is.read(mybytearray, 0, mybytearray.length);
-			current = bytesRead;
-
-			do {
-				bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
-				if (bytesRead >= 0)
-					current += bytesRead;
-			} while (bytesRead > -1);
-
-			bos.write(mybytearray, 0, current);
-			bos.flush();
-			System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
-			HandlerImage.readObject();
-
-		} finally
-
-		{
-			if (fos != null)
-				fos.close();
-			if (bos != null)
-				bos.close();
-			if (sock != null)
-				sock.close();
-		}
 	}
 
+	private static Runnable t1 = new Runnable() {
+		public void run() {
+			final int SOCKET_PORT = 13267; // you may change this
+			final String SERVER = "localhost"; // localhost
+			final String FILE_TO_RECEIVED = "\\Users\\nicol\\Desktop\\ImagemSerializedeucetocarai2.ser"; // you
+
+			final int FILE_SIZE = 6022386; // file size temporary hard coded
+			// should bigger than the file to be downloaded
+			int bytesRead;
+			int current = 0;
+			FileOutputStream fos = null;
+			BufferedOutputStream bos = null;
+			Socket sock = null;
+			try {
+				sock = new Socket(SERVER, SOCKET_PORT);
+
+				System.out.println("Connecting...");
+
+				// receive file
+				byte[] mybytearray = new byte[FILE_SIZE];
+				InputStream is = sock.getInputStream();
+				fos = new FileOutputStream(FILE_TO_RECEIVED);
+				bos = new BufferedOutputStream(fos);
+				bytesRead = is.read(mybytearray, 0, mybytearray.length);
+				current = bytesRead;
+
+				do {
+					bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
+					if (bytesRead >= 0)
+						current += bytesRead;
+				} while (bytesRead > -1);
+
+				bos.write(mybytearray, 0, current);
+				bos.flush();
+				System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
+				HandlerImage.readObject();
+
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally
+
+			{
+				try {
+					if (fos != null)
+						fos.close();
+
+					if (bos != null)
+						bos.close();
+
+					if (sock != null)
+						sock.close();
+				} catch (IOException e) {
+			
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
+	private static Runnable t2 = new Runnable() {
+		public void run() {
+			final int SOCKET_PORT = 4200; // you may change this
+	    	final String SERVER = "localhost"; // localhost
+	    	
+	       
+	        String hostName = SERVER;
+	        int portNumber = SOCKET_PORT;
+
+	        System.out.println("Cliente ECHO iniciado...");
+	        try (
+	            Socket echoSocket = new Socket(hostName, portNumber);
+	            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+	        ) {
+	            String userInput;
+	            System.out.println("Conectado ao Servidor.");
+	            System.out.println("Aguardando mensagem...");
+	            while ((userInput = in.readLine()) != null) {
+	                System.out.println("Mensagem recebida: " + userInput);
+	            }
+	        } catch (UnknownHostException e) {
+	            System.err.println("Ocorreu um erro ao tentar conectar ao servidor " + hostName);
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.err.println("Não foi possível conectar ao Servidor " + hostName);
+	            System.exit(1);
+	        } 
+	    
+			
+		}
+	};
+
 }
+
 /*
  * try (ServerSocket serverSocket = new ServerSocket(SOCKET_PORT); Socket
  * clientSocket = serverSocket.accept(); PrintWriter out = new
--- src/SimpleFileServer.java
+++ src/SimpleFileServer.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -17,43 +18,98 @@ public class SimpleFileServer {
 	public final static String FILE_TO_SEND = "\\Users\\nicol\\Desktop\\ImagemSerialize.ser"; // you may change this
 
 	public static void main() throws IOException {
-		
-		final String SERVER = "localhost";
-		FileInputStream fis = null;
-		BufferedInputStream bis = null;
-		OutputStream os = null;
-		ServerSocket servsock = null;
-		Socket sock = null;
+		new Thread(t1).start();
+		new Thread(t2).start();
+	}
+	private static Runnable t1 = new Runnable() {
+		public void run() {
+			final String SERVER = "localhost";
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			OutputStream os = null;
+			ServerSocket servsock = null;
+			Socket sock = null;
 
-		servsock = new ServerSocket(SOCKET_PORT);
-		while (true) {
-			System.out.println("Waiting...");
 			try {
-				sock = servsock.accept();
-				System.out.println("Accepted connection : " + sock);
-				// send file
-				File myFile = new File(FILE_TO_SEND);
-				byte[] mybytearray = new byte[(int) myFile.length()];
-				fis = new FileInputStream(myFile);
-				bis = new BufferedInputStream(fis);
-				bis.read(mybytearray, 0, mybytearray.length);
-				os = sock.getOutputStream();
-				System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
-				os.write(mybytearray, 0, mybytearray.length);
-				os.flush();
+				servsock = new ServerSocket(SOCKET_PORT);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			while (true) {
+				System.out.println("Waiting...");
+				try {
+					try {
+						sock = servsock.accept();
+						System.out.println("Accepted connection : " + sock);
+						// send file
+						File myFile = new File(FILE_TO_SEND);
+						byte[] mybytearray = new byte[(int) myFile.length()];
+						fis = new FileInputStream(myFile);
+						bis = new BufferedInputStream(fis);
+						bis.read(mybytearray, 0, mybytearray.length);
+						os = sock.getOutputStream();
+						System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+						os.write(mybytearray, 0, mybytearray.length);
+						os.flush();
 
-				System.out.println("Done.");
-				
+						System.out.println("Done.");
 
-			} finally {
-				if (bis != null)
-					bis.close();
-				if (os != null)
-					os.close();
-				if (sock != null)
-					sock.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} finally {
+					try {
+
+						if (bis != null)
+							bis.close();
+						if (os != null)
+							os.close();
+						if (sock != null)
+							sock.close();
+					}catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+
+
+					}
+				}
 			}
 		}
-	}
+	};
+	private static Runnable t2 = new Runnable() {
+		public void run() {
+
+
+
+			final int SOCKET_PORT = 4200; // you may change this
+
+			int portNumber = SOCKET_PORT;
+
+			System.out.println("Servidor ECHO pronto... Pressione CTRL+C para sair. \nAguardando o Cliente...");
+			try (
+					ServerSocket serverSocket = new ServerSocket(SOCKET_PORT);
+					Socket clientSocket = serverSocket.accept();
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+					) {
+				String inputLine;
+				System.out.println("Aguardando Mensagem a enviar...");
+				while ((inputLine = in.readLine()) != null) {
+					System.out.println("Mensagem enviada: " + inputLine);
+					out.println(inputLine);
+					System.out.println("Digite nova mensagem...");
+				}
+			} catch (IOException e) {
+				System.out.println("Erro detectado ao tentar ouvir a porta " + portNumber + " ou escutar a conexão.");
+				System.out.println(e.getMessage());
+			}
+
+		}
+	};
+
 
 }
+
+
